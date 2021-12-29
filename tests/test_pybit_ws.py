8321423b3aaa4677e882ed0ece9d651e4d6439b9
Jerry Boonstra <verysolvedsoftware@gmail.com>
2021-12-28 18:16:19 -0800

    fixup: according to the documentation, on_open passes one argument and on_close passes 3. Adding lambda wrappers to the default implementation of these so they actually run

--- tests/test_pybit_ws.py
+++ tests/test_pybit_ws.py
@@ -1,40 +1,12 @@
 import unittest
-import websocket
 
-try:
-    import thread
-except ImportError:
-    import _thread as thread
 import time
 import json
-import ssl
-import threading
 
 from pybit import WebSocket
 
-ws = WebSocket('wss://stream.bybit.com/realtime', subscriptions=['instrument_info.100ms.BTCUSD'])
 
-
-class WebSocketExample:
-    def __init__(self):
-        self.data = {}
-
-        websocket.enableTrace(True)
-        self.ws = websocket.WebSocketApp(
-            "wss://stream.bytick.com/realtime",
-            on_open=self._on_open,
-            on_message=self._on_message,
-            on_error=self._on_error,
-            on_close=self._on_close,
-        )
-        # Setup the thread running WebSocketApp.
-        self.wst = threading.Thread(target=lambda: self.ws.run_forever(
-            sslopt={"cert_reqs": ssl.CERT_NONE},
-        ))
-
-        # Configure as daemon; start.
-        self.wst.daemon = True
-        self.wst.start()
+class WebSocketExample(WebSocket):
 
     def orderbook(self):
         return self.data.get('orderBook_200.100ms.BTCUSD')
@@ -45,7 +17,7 @@ class WebSocketExample:
     # on_error=lambda ws, err: self._on_error(err)
 
     # noinspection PyUnusedLocal
-    def _on_message(self, our_ws, message):
+    def _on_message(self, message):
         m = json.loads(message)
         if 'topic' in m and m.get('topic') == 'orderBook_200.100ms.BTCUSD' and m.get(
                 'type') == 'snapshot':
@@ -53,16 +25,15 @@ class WebSocketExample:
             self.data[m.get('topic')] = m.get('data')
 
     # noinspection PyUnusedLocal
-    def _on_error(self, our_ws, error):
+    def _on_error(self, error):
         print(error)
 
-    def _on_close(self, our_ws, close_status_code, close_message):
-        print(
-            f"### closed {our_ws} with status_code={close_status_code} message={close_message} ###")
+    def _on_close(self, code, msg):
+        print(f"### closed {self.wsName} with status_code={code} message={msg} ###")
 
-    def _on_open(self, our_ws):
+    def _on_open(self, ws):
         print('Submitting subscriptions...')
-        our_ws.send(json.dumps({
+        ws.send(json.dumps({
             'op': 'subscribe',
             'args': ['orderBook_200.100ms.BTCUSD']
         }))
@@ -72,13 +43,16 @@ class WebSocketTest(unittest.TestCase):
 
     # A very simple test to ensure we're getting something from WS.
     def test_websocket_sanity(self):
+        ws = WebSocket('wss://stream.bybit.com/realtime',
+                       subscriptions=['instrument_info.100ms.BTCUSD'])
         self.assertNotEqual(
             ws.fetch('instrument_info.100ms.BTCUSD'),
             []
         )
 
     def test_websocket_success(self):
-        local_ws = WebSocketExample()
+        local_ws = WebSocketExample("wss://stream.bytick.com/realtime",
+                                    subscriptions=['instrument_info.100ms.BTCUSD'])
         time.sleep(5)
         # print(local_ws.orderbook())
         self.assertNotEqual(
