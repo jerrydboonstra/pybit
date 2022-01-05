82c92d5d696b27df00adf753ea605260d785ee37
Jerry Boonstra <verysolvedsoftware@gmail.com>
2022-01-04 17:35:23 -0800

    further tuning on test logging messages to include thread name and compress some log lines

--- tests/test_restructure_wss.py
+++ tests/test_restructure_wss.py
@@ -5,7 +5,8 @@ import unittest
 
 from pybit import usdt_perpetual, inverse_perpetual
 
-logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(levelname)s %(message)s")
+log_format = "%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s"
+logging.basicConfig(level=logging.DEBUG, format=log_format)
 logger = logging.getLogger('test')
 
 API_KEY = os.environ.get('BYBIT_API_KEY', '')
@@ -13,18 +14,15 @@ API_SECRET = os.environ.get('BYBIT_API_SECRET', '')
 
 
 def instrument_info_callback(message):
-    logger.debug(f"instrument_info_callback()")
-    logger.info(message)
+    logger.info("instrument_info_callback got message: %s", message)
 
 
 def orderbook_callback(message):
-    logger.debug(f"orderbook_callback()")
-    logger.info(message)
+    logger.info("orderbook_callback got message: %s", message)
 
 
 def position_callback(message):
-    logger.debug(f"position_callback()")
-    logger.info(message)
+    logger.info("position_callback got message: %s", message)
 
 
 class HTTPTest(unittest.TestCase):
@@ -35,7 +33,7 @@ class HTTPTest(unittest.TestCase):
         while i < loops:
             i = i + 1
             sleep(sleep_secs)
-            logger.info(f"Polling {i * sleep_secs}s out of {loops * sleep_secs}s")
+            logger.debug(f"Sleeping {i * sleep_secs}s out of {loops * sleep_secs}s")
 
     def test_instrument_info(self):
         # given
